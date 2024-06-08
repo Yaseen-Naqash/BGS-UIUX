b6460c772e5104ee04a2e580a0f2b135589162a6
yaseen <yaseen.naqqash@gmail.com>
2024-06-09 01:55:29 +0430

    navbar halfway

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,8 @@ document.addEventListener('DOMContentLoaded', () => {
 // slides[currentIndex].classList.add('active');
 // setInterval(showNextSlide, 7000);
 
+
+// search animation
 $(document).ready(function(){
     console.log('JavaScasdasdasdript Loaded');
     $("#search").focus(function() {
@@ -36,4 +38,39 @@ $(document).ready(function(){
     $(".go-icon").click(function(){
       $(".search-form").submit();
     });
+});
+
+
+// navbar slide
+document.addEventListener('DOMContentLoaded', function() {
+  const toggleButton = document.getElementById('navbar-toggle');
+  const sidebar = document.createElement('div');
+  sidebar.classList.add('sidebar');
+
+  const closeButton = document.createElement('span');
+  closeButton.classList.add('close-btn');
+  closeButton.innerHTML = '&times;';
+  sidebar.appendChild(closeButton);
+
+
+  const navbarMenu = document.querySelector('.navbar-links');
+  const clonedMenu = document.createElement('ul');
+  
+  navbarMenu.querySelectorAll('li').forEach(item => {
+    if (!item.classList.contains('no-sidebar')) {
+      const clonedItem = item.cloneNode(true);
+      clonedMenu.appendChild(clonedItem);
+    }
+  });
+
+  sidebar.appendChild(clonedMenu);
+  document.body.appendChild(sidebar);
+
+  toggleButton.addEventListener('click', function() {
+    sidebar.classList.toggle('active');
+  });
+
+  closeButton.addEventListener('click', function() {
+    sidebar.classList.remove('active');
+  });
 });
